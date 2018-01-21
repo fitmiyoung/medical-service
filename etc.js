fde96906994c578d2899a128fd29d726b633904d
miyoungyoon <fitmiyoung@gmail.com>
2018-01-21 23:58:14 +0900

    서브 메뉴 5가지 이름, 순서 변경 로그인 헤드 추가, 예시 텍스트 입력 유전체 결과 변경, 새로운 표로 디자인 리스트 검색 옵션추가 (ID,날짜) 프로필: 변경된 서브 메뉴명과 순서로 변경

--- etc.js
+++ etc.js
@@ -1,15 +1,9 @@
-// $(".m-sub-1").html("<a href="sub_2_upload.html"><i class="fa fa-picture-o" aria-hidden="true"></i>세포 명시야 현미경 영상</a>");
-// $(".m-sub-2").html("<a href="sub_1_upload.html"><i class="fa fa-picture-o" aria-hidden="true"></i>세포 형광 현미경 영상</a>");
-// $(".m-sub-3").html("<a href="sub_3_upload.html"><i class="fa fa-picture-o" aria-hidden="true"></i>유전체 데이터</a>");
-// $(".m-sub-4").html("<a href="sub_5_upload.html"><i class="fa fa-picture-o" aria-hidden="true"></i>의료 영상 데이터(MRI)</a>");
-// $(".m-sub-5").html("<a href="sub_4_upload.html"><i class="fa fa-picture-o" aria-hidden="true"></i>의료 영상 판독문</a>");
-
-
-$(".m-sub-1").html("세포 명시야 현미경 영상").contents().wrap('<a href="sub_2_upload.html" class="fa fa-picture-o"></a>');
-$(".m-sub-2").html("세포 형광 현미경 영상").contents().wrap('<a href="sub_1_upload.html" class=""></a>').addClass("fa fa-picture-o");
-$(".m-sub-3").html("유전체 데이터").contents().wrap('<a href="sub_3_upload.html" class="fa fa-picture-o"></a>');
-$(".m-sub-4").html("의료 영상 데이터 (MRI)").contents().wrap('<a href="sub_5_upload.html" class="fa fa-picture-o"></a>');
-$(".m-sub-5").html("의료 영상 판독문").contents().wrap('<a href="sub_4_upload.html" class="fa fa-picture-o"></a>');
+// sub-menu
+$(".m-sub-1").html("세포 명시야 현미경 영상").contents().wrap('<a href="sub_2_upload.html"> </a>');
+$(".m-sub-2").html("세포 형광 현미경 영상").contents().wrap('<a href="sub_1_upload.html"></a>');
+$(".m-sub-3").html("의료 영상 데이터 (MRI)").contents().wrap('<a href="sub_5_upload.html"></a>');
+$(".m-sub-4").html("의료 영상 판독문").contents().wrap('<a href="sub_4_upload.html"></a>');
+$(".m-sub-5").html("유전체 데이터").contents().wrap('<a href="sub_3_upload.html"></a>');
 
 
 
